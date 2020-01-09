c5a7eb9e05cdd8463c581e555aa534e41f24c99a
Vladim1r37 <vl4d1m1r37@gmail.com>
2020-01-10 03:00:16 +0400

    project files commited

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -0,0 +1,57 @@
+package ru.skillbranch.skillarticles.ui.custom.behaviors
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.snackbar.Snackbar
+import kotlin.math.max
+import kotlin.math.min
+
+class BottombarBehavior<V : View> @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : CoordinatorLayout.Behavior<V>(context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean =
+        axes == ViewCompat.SCROLL_AXIS_VERTICAL
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+    }
+
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: V, dependency: View): Boolean {
+        if (dependency is Snackbar.SnackbarLayout)
+            updateSnackbar(child, dependency)
+        return super.layoutDependsOn(parent, child, dependency)
+    }
+
+    private fun updateSnackbar(child: V, snackbarLayout: Snackbar.SnackbarLayout) {
+        if (snackbarLayout.layoutParams is CoordinatorLayout.LayoutParams) {
+            val params = snackbarLayout.layoutParams as CoordinatorLayout.LayoutParams
+
+            params.anchorId = child.id
+            params.anchorGravity = Gravity.TOP
+            params.gravity = Gravity.TOP
+            snackbarLayout.layoutParams = params
+        }
+    }
+}
