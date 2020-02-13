0de7e7b508ff02063fe42d8e11a4591204501afa
Vladim1r37 <vl4d1m1r37@gmail.com>
2020-02-13 09:03:13 +0400

    realisation of behaviors changed

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -5,19 +5,21 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.math.MathUtils
 import androidx.core.view.ViewCompat
 import com.google.android.material.snackbar.Snackbar
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
 import kotlin.math.max
 import kotlin.math.min
 
-class BottombarBehavior<V : View> @JvmOverloads constructor(
+class BottombarBehavior @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
-) : CoordinatorLayout.Behavior<V>(context, attrs) {
+) : CoordinatorLayout.Behavior<Bottombar>(context, attrs) {
 
     override fun onStartNestedScroll(
         coordinatorLayout: CoordinatorLayout,
-        child: V,
+        child: Bottombar,
         directTargetChild: View,
         target: View,
         axes: Int,
@@ -27,31 +29,32 @@ class BottombarBehavior<V : View> @JvmOverloads constructor(
 
     override fun onNestedPreScroll(
         coordinatorLayout: CoordinatorLayout,
-        child: V,
+        child: Bottombar,
         target: View,
         dx: Int,
         dy: Int,
         consumed: IntArray,
         type: Int
     ) {
+        val offset = MathUtils.clamp(child.translationY + dy, 0f, child.minHeight.toFloat())
+        if (offset != child.translationY) child.translationY = offset
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
-        child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
     }
 
-    override fun layoutDependsOn(parent: CoordinatorLayout, child: V, dependency: View): Boolean {
-        if (dependency is Snackbar.SnackbarLayout)
-            updateSnackbar(child, dependency)
-        return super.layoutDependsOn(parent, child, dependency)
-    }
-
-    private fun updateSnackbar(child: V, snackbarLayout: Snackbar.SnackbarLayout) {
-        if (snackbarLayout.layoutParams is CoordinatorLayout.LayoutParams) {
-            val params = snackbarLayout.layoutParams as CoordinatorLayout.LayoutParams
-
-            params.anchorId = child.id
-            params.anchorGravity = Gravity.TOP
-            params.gravity = Gravity.TOP
-            snackbarLayout.layoutParams = params
-        }
-    }
+//    override fun layoutDependsOn(parent: CoordinatorLayout, child: Bottombar, dependency: View): Boolean {
+//        if (dependency is Snackbar.SnackbarLayout)
+//            updateSnackbar(child, dependency)
+//        return super.layoutDependsOn(parent, child, dependency)
+//    }
+//
+//    private fun updateSnackbar(child: Bottombar, snackbarLayout: Snackbar.SnackbarLayout) {
+//        if (snackbarLayout.layoutParams is CoordinatorLayout.LayoutParams) {
+//            val params = snackbarLayout.layoutParams as CoordinatorLayout.LayoutParams
+//
+//            params.anchorId = child.id
+//            params.anchorGravity = Gravity.TOP
+//            params.gravity = Gravity.TOP
+//            snackbarLayout.layoutParams = params
+//        }
+//    }
 }
--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -1,45 +1,34 @@
 package ru.skillbranch.skillarticles.ui.custom.behaviors
 
-import android.content.Context
-import android.util.AttributeSet
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.view.ViewCompat
+import androidx.core.view.marginRight
 import ru.skillbranch.skillarticles.ui.custom.ArticleSubmenu
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
 
-class SubmenuBehavior @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null
-) : CoordinatorLayout.Behavior<ArticleSubmenu>(context, attrs) {
-    var isHidden = false
+class SubmenuBehavior : CoordinatorLayout.Behavior<ArticleSubmenu>() {
 
-    override fun onStartNestedScroll(
-        coordinatorLayout: CoordinatorLayout,
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
         child: ArticleSubmenu,
-        directTargetChild: View,
-        target: View,
-        axes: Int,
-        type: Int
-    ): Boolean =
-        axes == ViewCompat.SCROLL_AXIS_VERTICAL
+        dependency: View
+    ): Boolean {
+        return dependency is Bottombar
+    }
 
-    override fun onNestedPreScroll(
-        coordinatorLayout: CoordinatorLayout,
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
         child: ArticleSubmenu,
-        target: View,
-        dx: Int,
-        dy: Int,
-        consumed: IntArray,
-        type: Int
-    ) {
-        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
-        if (dy > 0 && child.isOpen && !isHidden) {
-            child.hide()
-            isHidden = true
-        }
-        if (dy < 0 && isHidden) {
-            child.show()
-            isHidden = false
-        }
+        dependency: View
+    ): Boolean {
+        return if (child.isOpen && dependency is Bottombar && dependency.translationY >= 0) {
+            animate(child, dependency)
+            true
+        } else false
+    }
+
+    private fun animate(child: ArticleSubmenu, dependency: Bottombar) {
+        val fraction = dependency.translationY / dependency.minHeight
+        child.translationX = (child.width + child.marginRight) * fraction
     }
 }
